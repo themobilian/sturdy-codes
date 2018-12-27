ab69c2fad2fa2188fe0205d85266ea210a8ad203
themobilian <vyshakh.hc@gmail.com>
2018-12-27 13:51:18 +0530

    Initial upload of CodeChef problem solutions

---
+++ Chef/FLOW006/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CChef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int[] N_Array = new int[T];
+            for (int i = 0; i < T; i++)
+            {
+                N_Array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < T; i++)
+            {
+                int T_Sum = 0;
+                while (N_Array[i] != 0)
+                {
+                    T_Sum += N_Array[i] % 10;
+                    N_Array[i] = N_Array[i] / 10;
+                }
+                Console.WriteLine(T_Sum);
+            }
+        }
+    }
+}
---
+++ Chef/TLG/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CChef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int lead = 0, p1=0,p2=0, p1maxlead=0,p2maxlead=0;
+            while (N-->0)
+            {
+                string[] scores = Console.ReadLine().Split();
+                p1 += int.Parse(scores[0]);
+                p2 += int.Parse(scores[1]);
+
+                lead = p1 - p2;
+                if (lead < 0)
+                {
+                    lead = -lead;
+                    if (p2maxlead < lead)
+                    {
+                        p2maxlead = lead;
+                    }
+                }
+                else
+                {
+                    if (p1maxlead < lead)
+                    {
+                        p1maxlead = lead;
+                    }
+                }
+            }
+            if (p1maxlead < p2maxlead)
+            {
+                Console.WriteLine("{0} {1}", 2, p2maxlead);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}", 1, p1maxlead);
+            }
+        }
+    }
+}
