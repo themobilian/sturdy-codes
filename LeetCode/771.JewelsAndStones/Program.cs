8aed9e4e38930e56063fd718daada6e34603c089
themobilian <vyshakh.hc@gmail.com>
2019-01-02 23:50:13 +0530

    Added solution for problems 771,961 and 962

---
+++ LeetCode/771.JewelsAndStones/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters.Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+*/    
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _771.JewelsAndStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string J = "aA";
+            string S = "aAAbbbb";
+            Console.WriteLine("{0}", new Program().NumJewelsInStones(J, S));
+            Console.ReadLine();
+        }
+        //string s = S.Where(c => !J.ToArray().Contains(c)).ToString();
+
+        public int NumJewelsInStones(string J, string S)
+        {
+            StringBuilder actualStones = new StringBuilder(S);
+            foreach (char stone in S.ToCharArray())
+            {
+                if (!J.Contains(stone))
+                {
+                    actualStones.Replace(stone.ToString(), "");
+                }
+            }
+            return actualStones.Length;
+        }
+
+    }
+}
---
+++ LeetCode/961.NRepeatedElement/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+
+Return the element repeated N times.
+
+ 
+Example 1:
+
+Input: [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: [5,1,5,2,5,3,5,4]
+Output: 5
+ 
+Note:
+
+4 <= A.length <= 10000
+0 <= A[i] < 10000
+A.length is even
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _961.NRepeatedElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[]{ 1, 2, 3, 3 };
+            Console.WriteLine("{0}",new Program().RepeatedNTimes(A));
+            Console.ReadLine();
+        }
+        public int RepeatedNTimes(int[] A)
+        {
+            int repeatedNum = A[0];
+            int len = A.Length;
+            bool isFound = false;
+            for (int i = 0; i < len; i++)
+            {
+                if (!isFound)
+                {
+                    for (int j = i+1; j < len; j++)
+                    {
+                        if (A[i] == A[j])
+                        {
+                            repeatedNum = A[i];
+                            isFound = true;
+                            break;
+                        }
+                    }
+                }
+                else { break; }
+            }
+            return repeatedNum;
+        }
+    }
+}
