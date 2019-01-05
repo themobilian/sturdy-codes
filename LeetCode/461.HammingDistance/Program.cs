50db9204cde7ae6f2d7e7a383f7d026683103bef
themobilian <vyshakh.hc@gmail.com>
2019-01-06 01:19:28 +0530

    Added solution for 461 and 657

---
+++ LeetCode/461.HammingDistance/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+Given two integers x and y, calculate the Hamming distance.
+Note:
+0 ≤ x, y < 231.
+
+Example:
+Input: x = 1, y = 4
+
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _461.HammingDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 5;
+            Console.WriteLine("{0}", new Program().HammingDistance(x,y));
+            Console.ReadLine();
+        }
+        public int HammingDistance(int x, int y)
+        {
+            int xor = x ^ y;
+            int distance = 0;
+            while (xor > 0)
+            {
+
+                distance += xor & 1;
+                xor = xor >> 1;
+            }
+            return distance;
+        }
+    }
+}
